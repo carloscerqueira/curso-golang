c2e524f5fc00da0a6fc326f6dd8a2994bef7d22e
Carlos Augusto Cerqueira Cunha <carloscerqueiracunha@gmail.com>
2024-09-16 21:20:58 -0300

    Resolucao do quarto exercicio da semana 2

---
+++ exercicios/semana2/04-exercicio/04-ex.go
@@ -0,0 +1,12 @@
+/* 4. Implemente um contador regressivo de 10 até 1 usando um laço for,
+e saia do laço quando atingir 0. */
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 10; i > 0; i-- {
+		fmt.Println(i)
+	}
+}
